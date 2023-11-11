d024f90419ccf3acaa6b7e3b0a5f9ab0b71aeeec
uozumi212 <kazu3278mana@gmail.com>
2023-11-11 14:22:23 +0900

    techlog

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Homes', type: :system do
   before do
-    driven_by(:rack_test)
-  end
-
-  before do
     driven_by :selenium_chrome_headless
   end
 
@@ -18,5 +14,51 @@ RSpec.describe 'Homes', type: :system do
 
   # pending "add some scenarios (or delete) #{__FILE__}"
    end
+  end
+  describe 'ナビゲーションバーの検証' do
+    context 'ログインしていない場合' do
+      before { visit '/' }
+
+      it 'ユーザー登録リンクを表示する' do
+        expect(page).to have_link('ユーザー登録', href: '/users/sign_up')
+      end
+
+      it 'ログインリンクを表示する' do
+        expect(page).to have_link('ログイン', href: '/users/sign_in')
+      end
+
+      it 'ログアウトリンクは表示しない' do
+        expect(page).not_to have_content('ログアウト')
+      end
+    end
+
+    context 'ログインしている場合' do
+      before do
+        user = create(:user) # ログイン用のユーザーを作成
+        sign_in user # 作成したユーザーでログイン
+        visit '/'
+      end
+
+      it 'ユーザー登録リンクは表示しない' do
+        expect(page).not_to have_link('ユーザー登録', href: '/users/sign_up')
+      end
+
+      it 'ログインリンクは表示しない' do
+        expect(page).not_to have_link('ログイン', href: '/users/sign_in')
+      end
+
+      it 'ログアウトリンクを表示する' do
+        expect(page).to have_content('ログアウト')
+      end
+
+      it 'ログアウトリンクが機能する' do
+        click_button 'ログアウト'
+
+        # ログインしていない状態のリンク表示パターンになることを確認
+        expect(page).to have_link('ユーザー登録', href: '/users/sign_up')
+        expect(page).to have_link('ログイン', href: '/users/sign_in')
+        expect(page).not_to have_content('ログアウト')
+      end
+    end
   end
 end

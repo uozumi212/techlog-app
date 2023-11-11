9b37e18fc1ce20b8dc683dceff488cb11a6e6726
uozumi212 <kazu3278mana@gmail.com>
2023-11-11 21:18:36 +0900

    Postモデルを作成

---
+++ db/migrate/20231111061310_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.string :content, null: false
+      t.references :user, foregin_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_100427) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_061310) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
